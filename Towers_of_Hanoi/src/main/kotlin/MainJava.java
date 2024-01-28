b5675042ead0dbbf1109b8c0efb0639667383fef
tran duy quyen <quyentranduy54321@gmail.com>
2024-01-28 09:33:00 +0700

    Commit changes

---
+++ Towers_of_Hanoi/src/main/kotlin/MainJava.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Scanner;
+
+public class MainJava {
+    public static void hanoiTower(int n, int start, int end){
+        if(n == 1){
+            System.out.println(start + " -> " + end);
+        }
+        else{
+            int otherRod = 6 - (start + end);
+            hanoiTower(n - 1, start, otherRod);
+            System.out.println(start + " -> " + end);
+            hanoiTower(n - 1, otherRod, end);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        hanoiTower(n, 1, 3);
+    }
+}
